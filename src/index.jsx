6c485371fcd5c2e8e9721cd33269331e9ec3675f
rmani07 <107785259+rmani07@users.noreply.github.com>
2022-07-23 21:41:45 +0530

    test
    tsets
    
    Co-Authored-By: RMani <48157481+rmani@users.noreply.github.com>

---
+++ src/index.jsx
@@ -0,0 +1,47 @@
+import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from "@forge/ui";
+import { storage, startsWith } from '@forge/api';
+
+await storage.query()
+  // Filter the response to only keys that start with the string 'value' test testsetsets
+  .where('key', startsWith('value'))
+
+  // Limit the result size to 10 values
+  .limit(10)
+
+  // Use the cursor provided (returned from a previous invocation)
+  .cursor('...')
+
+  // Get a list of results
+  .getMany();
+
+const fetchCommentsForIssue = async (issueId) => {
+  const res = await api
+    .asUser()
+    .requestJira(route`/rest/api/3/issue/${issueId}/comment`);
+
+  const data = await res.json();
+  return data.comments;
+};
+
+const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
+
+  console.log(`Number of comments on this issue: ${comments.length}`);
+
+  return (
+    <Fragment>
+      <Text>Hello world!</Text>
+      <Text>
+        Number of comments on this issue: {comments.length}
+      </Text>
+  </Fragment>
+  );
+};
+
+export const run = render(
+  <IssuePanel>
+    <App />
+  </IssuePanel>
+);
